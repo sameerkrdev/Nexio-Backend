cf303a2be34343b6f398372e7b94bd8c16c52834
Sameer Kumar <sameerkr.dev@gmail.com>
2026-05-01 01:35:49 +0530

    feat: add winston logger

---
+++ src/config/logger.ts
@@ -0,0 +1,32 @@
+import winston, { format } from 'winston';
+const { combine, timestamp, json } = format;
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()), // production format
+  transports: [
+    // file for errors
+    new winston.transports.File({
+      dirname: 'logs',
+      filename: 'error.log',
+      level: 'error',
+    }),
+
+    // file for all logs
+    new winston.transports.File({
+      dirname: 'logs',
+      filename: 'combined.log',
+    }),
+  ],
+});
+
+// dev console transport
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: combine(json(), timestamp()),
+    }),
+  );
+}
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import logger from './config/logger';
 
 const app = express();
 
@@ -7,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  logger.info('Server running on port 3000');
 });
